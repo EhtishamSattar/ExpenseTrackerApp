f07cc45ec8e45360c0ad6c76599c53c682420d2b
EhtishamSattar <ehtishamsattar888@gmail.com>
2024-08-27 17:01:45 +0500

    Removing business logic from views

---
+++ ExpenseTrackerApp/Modules/Home/Views/Expense/ExpenseButtons/ExpenseDeleteButton.swift
@@ -0,0 +1,18 @@
+//
+//  ExpenseDeleteButton.swift
+//  ExpenseTrackerApp
+//
+//  Created by Mac on 27/08/2024.
+//
+
+import SwiftUI
+
+struct ExpenseDeleteButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExpenseDeleteButton()
+}
